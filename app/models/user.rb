188b61e294b95c8cc685ec9b1789e681fede7b80
Tech-A46 <tech-a46@Tech-a46s-MacBook-Air.local>
2018-10-04 16:05:44 +0100

    added comments controller, views, routes and models and linked to blogs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
  has_many :blogs, dependent: :destroy
+ has_many :comments, dependent: :destroy
    def full_name
      "#{first_name} #{last_name}"
    end
