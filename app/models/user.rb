fbcb02f601a27ce754b9a0c52f1a7afbfe4a5b94
Tech-A46 <tech-a46@Tech-a46s-MacBook-Air.local>
2018-10-04 14:43:06 +0100

    created devise controller and comments controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
  has_many :blogs, dependent: :destroy
+   def full_name
+     "#{first_name} #{last_name}"
+   end
 end
